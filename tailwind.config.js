cedaa5cd0a9ec0afebf66c05447682bd433f572f
blueheart <2321837783@qq.com>
2022-01-20 17:36:47 +0800

    project initial

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/*
+ * @Description: 
+ * @Autor: Blueheart
+ * @Date: 2022-01-20 17:10:25
+ * @LastEditTime: 2022-01-20 17:13:47
+ * @FilePath: \blueheart_blog\tailwind.config.js
+ */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
